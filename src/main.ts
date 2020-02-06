3dc77041017975110d18f6847e850f1d5f113db4
Christian Griffin <c.griffin@vehikl.com>
2020-02-06 10:29:59 -0500

    add Google Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import VueYouTubeEmbed from 'vue-youtube-embed';
+import VueAnalytics from 'vue-analytics';
 
 Vue.config.productionTip = false;
 
 Vue.use(VueYouTubeEmbed);
+Vue.use(VueAnalytics, {id: 'UA-73783760-4'});
 
 new Vue({
   router,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,17 +8,20 @@
     </div>
 </template>
 
-<script>
+<script lang="ts">
     import {Component, Vue} from 'vue-property-decorator';
-    import HeroHeader from '@/components/HeroHeader';
-    import BadCinematics from '@/components/BadCinematics';
-    import CutFeatures from '@/components/CutFeatures';
-    import AntiConsumer from '@/components/AntiConsumer';
-    import MapOwnership from '@/components/MapOwnership';
+    import HeroHeader from '@/components/HeroHeader.vue';
+    import BadCinematics from '@/components/BadCinematics.vue';
+    import CutFeatures from '@/components/CutFeatures.vue';
+    import AntiConsumer from '@/components/AntiConsumer.vue';
+    import MapOwnership from '@/components/MapOwnership.vue';
 
     @Component({
         components: {HeroHeader, CutFeatures, BadCinematics, AntiConsumer, MapOwnership},
     })
     export default class Home extends Vue {
+        public track() {
+            this.$ga.page('/');
+        }
     }
 </script>
